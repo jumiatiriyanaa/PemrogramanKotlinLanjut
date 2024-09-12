b38f6831d6ef94e4b5c08ac70e0fb3061b4bd5bc
Jumiati Riyana <jumiatiriyanaa@gmail.com>
2024-09-12 10:32:14 +0700

    Add NullSafeCalss.kt

---
+++ src/main/kotlin/NullSafeCalls.kt
@@ -0,0 +1,9 @@
+fun main() {
+    //safe calling
+    //?.let
+    var maybeNumber: Int? = 15
+    maybeNumber?.let { print(it) }
+
+    //The ?. chaining
+    //Student?.teacher?.suervisor?.name = "Seymour Skinner
+}
