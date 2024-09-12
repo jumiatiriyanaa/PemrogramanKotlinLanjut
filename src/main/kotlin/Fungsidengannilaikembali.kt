c351926efc184b6bf51680f99c42979a4d1d54e2
Jumiati Riyana <jumiatiriyanaa@gmail.com>
2024-09-12 11:13:58 +0700

    Add FungsidengannilaiKembali.kt

--- src/main/kotlin/Fungsidengannilaikembali.kt
+++ src/main/kotlin/Fungsidengannilaikembali.kt
@@ -1,6 +1,6 @@
-fun main() {
-    val lisNumber = listOf(1,2,3,4,5,6)
-    //println("Jumlah nilai pada elemen listNumber = ${getSum(listNumber)}")
+fun main(args: Array<String>) {
+    val listNumber = listOf(1,2,3,4,5,6)
+    println("Jumlah nilai pada elemen listNumber = ${getSum(listNumber)}")
 }
 
 fun getSum(values: List<Int>) : Int { // return type is Int
